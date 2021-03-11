40a664d794808495e7bc99dfeb8d782203059e1b
Kaindra Djoemena <62385409+KaindraDjoemena@users.noreply.github.com>
2021-03-11 15:23:42 +0700

    Update main.py

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,25 +1,37 @@
+# Importing all the modules
 import os
 import sys
 import time
+import pyperclip
 from encrypter import encrypt
 from decrypter import decrypt
 from password_generator import generatePassword
 
+# Program class
 class Program:
+	# Clears the window
 	def clear():
 		os.system("cls")
-	
+
+	# Quits the program	
 	def quit():
 		quit()
 
+	# Delays a command
 	def delay(t = 1):
 		time.sleep(t)
 
+	# Copies x
+	def copy(x):
+		pyperclip.copy(x)
+
+	# Checking if the newly made password is valid or not
 	def passwordIsValid(password, confirmation):
 		if password == confirmation:
 			if len(password) >= 10:
 				return True
 
+	# Making a new password
 	def makeMasterPassword():
 		print("  lets get started")
 		input("\nreturn key to continue: ")
@@ -37,7 +49,7 @@ class Program:
 				print("invalid input.")
 			if (input_continue == "Y"):
 				if (Program.passwordIsValid(input_make_password, input_confirmation_password)):
-					master_password_database.write(encrypt(input_make_password))
+					master_password_database.write(encrypt(input_make_password)) # Encrypts the password
 					print("welcome")
 					input("\nreturn key to continue: ")
 					Program.quit()
@@ -47,6 +59,7 @@ class Program:
 			elif (input_continue == "N"):
 				Program.quit()
 
+	# Reads a line of a file
 	def readLines(database_file):
 		lines_database = database_file.readlines()
 		line_list = []
@@ -54,18 +67,22 @@ class Program:
 			line_list.append(line.strip("\n"))
 		return line_list
 
+	# Writes a line to a file
 	def writeLine(file, line):
-		written_line = file.write(line + "\n")
+		written_line = file.write(encrypt(line) + "\n")
 		return written_line
 
+	# Displays all of the slots/info
 	def display():
 		for line in range(len(website_list)):
-			print(" website  : {}".format(website_list[line]))
-			print(" url      : {}".format(url_list[line]))
-			print(" username : {}".format(username_list[line]))
+			# Decrypts the encrypted password
+			print(" website  : {}".format(decrypt(website_list[line])))
+			print(" url      : {}".format(decrypt(url_list[line])))
+			print(" username : {}".format(decrypt(username_list[line])))
 			print(" password : {}".format(decrypt(password_list[line])))
 			print()
 
+	# Makes a new slot
 	def newSlot():
 		website_input  = input(" website  : ")
 		url_input      = input(" url      : ")
@@ -79,15 +96,16 @@ class Program:
 				Program.writeLine(url_database, url_input)
 				Program.writeLine(username_database, username_input)
 				if password_input == "/r":
-					Program.writeLine(password_database, encrypt(generatePassword()))
+					Program.writeLine(password_database, generatePassword())
 				else:
-					Program.writeLine(password_database, encrypt(password_input))
+					Program.writeLine(password_database, password_input)
 				print("action successful.")
 				break
 			elif confirmation == "N":
 				print("action cancelled.")
 				break
 
+	# Saves/closes the file to save it
 	def save():
 		master_password_database.close()
 		username_database.close()
@@ -96,12 +114,14 @@ class Program:
 		url_database.close()
 
 
+# Opens the files
 master_password_database = open("master_password_database.txt", "r+")
 username_database        = open("username_database.txt", "r+")
 password_database        = open("password_database.txt", "r+")
 website_database         = open("website_database.txt", "r+")
 url_database             = open("url_database.txt", "r+")
 
+# Read the lines
 username_list   = Program.readLines(username_database)
 password_list   = Program.readLines(password_database)
 website_list    = Program.readLines(website_database)
@@ -110,17 +130,20 @@ master_password = Program.readLines(master_password_database)
 
 Program.clear()
 
+# If there is no master password/if its a new account, then we call the makeMasterPassword() method
 if master_password == []:
 	Program.makeMasterPassword()
 
+# Loops forever
 while True:
-	master_password_input = input("enter master password: ")
-	if encrypt(master_password_input) == master_password[0]:
-		Program.clear()
-		while True:
-			user_input = input("h]>>")
-			if user_input == "/d":
-				Program.clear()
+	# Asks for the master password
+	master_password_input = input("enter master password: ")		# /h = home page
+	if encrypt(master_password_input) == master_password[0]:		# /d = displays the info/goes to display page
+		Program.clear()												# // = back a page
+		while True:													# /n = makes a new slot
+			user_input = input("h]>>")								# /c = clears the window
+			if user_input == "/d":									# /q = quits the program
+				Program.clear()										# /r = generates a random password
 				while True:
 					user_input = input("h]d]>>")
 					if user_input == "//":
