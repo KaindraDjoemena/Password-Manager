246f39df4190fe056e92decb638d5c673fb00972
Kaindra Djoemena <62385409+KaindraDjoemena@users.noreply.github.com>
2021-03-15 08:15:11 +0700

    Update encrypter.py

--- password-manager/encrypter.py
+++ password-manager/encrypter.py
@@ -1,6 +1,3 @@
-# message encrypter
-
-# import pyperclip
 from encryption_dict_file import *
 
 def encrypt(message):
@@ -10,11 +7,3 @@ def encrypt(message):
 		conv_char = conv_dict[char]
 		encrypted_word += conv_char
 	return encrypted_word
-
-# print("[Enter your message to encrypt]: ")
-# word = input(">>")
-# encrypted_word = encrypt(word)
-# print("\n[Encrypted]: ")
-# print(encrypted_word)
-# pyperclip.copy(encrypted_word)
-# print("\ncopied to clipboard!")
