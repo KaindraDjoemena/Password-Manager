27dc7aef8cc37628571148fe6bbb0682a0063a71
Kaindra Djoemena <62385409+KaindraDjoemena@users.noreply.github.com>
2021-03-15 18:00:54 +0700

    pickling + new encryption system

--- password-manager/main.py
+++ password-manager/main.py
@@ -2,15 +2,19 @@
 import os
 import time
 import hashlib
+import pickle
 import pyperclip
-from encrypter import encrypt
-from decrypter import decrypt
+from cryptography import Crypto
 from password_generator import generatePassword
 
+crypto = Crypto() # < Initializing an object "crypto" from the class "Crypto()"
+
+# The program function
 def program(in_program):
 
 	# Program class
 	class Program:
+
 		# Clears the window
 		def clear():
 			os.system("cls")
@@ -27,10 +31,34 @@ def program(in_program):
 		def copy(x):
 			pyperclip.copy(x)
 
+        # Hashes the password with SHA512
 		def hash(password):
 			result = hashlib.sha512(password.encode())
 			hashed_password = result.hexdigest()
 			return hashed_password
+
+		# Makes the encryption key and pickles it
+		def makeKey():
+			key = crypto.makeKey()
+			key_file = open("key_file.pickle", "wb")
+			pickle.dump(key, key_file)
+			key_file.close()
+
+		# Opens the encrypted key
+		def openEncKey():
+			key_file = open("key_file.pickle", "rb")
+			key_dict = pickle.load(key_file)
+			return key_dict
+
+		# Encrypts the data
+		def encrypt(x, key_file):
+			encrypted = crypto.encrypt(x, key_file)
+			return encrypted
+
+		# Decrypts the encrypted data
+		def decrypt(x, key_file):
+			decrypted = crypto.decrypt(x, key_file)
+			return decrypted
 
 		# Restarts the program
 		def restart(user_state):
@@ -102,6 +130,7 @@ def program(in_program):
 						master_password_database.write((Program.hash(input_make_password)) + "\n") # Encrypts the password
 						print("welcome")
 						input("\nreturn key to continue: ")
+						Program.makeKey()
 						Program.quit()
 					else:
 						Program.clear()
@@ -109,18 +138,19 @@ def program(in_program):
 				elif (input_continue == "N"):
 					Program.quit()
 
+
 		# Writes a line to a file
-		def writeLine(file, line):
-			written_line = file.write(encrypt(line) + "\n")
+		def writeLine(file, line, key_dict):
+			written_line = file.write(Program.encrypt(line, key_dict) + "\n")
 
 		# Displays all of the slots/info
 		def display():
 			for line in range(len(website_list)):
 				# Decrypts the encrypted password
-				print(" website  : {}".format(decrypt(website_list[line])))
-				print(" url      : {}".format(decrypt(url_list[line])))
-				print(" username : {}".format(decrypt(username_list[line])))
-				print(" password : {}".format(decrypt(password_list[line])))
+				print(" website  : {}".format(Program.decrypt(website_list[line], key_dict)))
+				print(" url      : {}".format(Program.decrypt(url_list[line], key_dict)))
+				print(" username : {}".format(Program.decrypt(username_list[line], key_dict)))
+				print(" password : {}".format(Program.decrypt(password_list[line], key_dict)))
 				print()
 
 		# Saves/closes the file to save it
@@ -142,13 +172,15 @@ def program(in_program):
 				# The program will write the input if the user chooses to and restarts the program
 				confirmation = input("\ncontinue?(Y/N): ").upper()
 				if confirmation == "Y":
-					Program.writeLine(website_database, website_input)
-					Program.writeLine(url_database, url_input)
-					Program.writeLine(username_database, username_input)
+					key_file = open("key_file.pickle", "rb")
+					key_dict = pickle.load(key_file)
+					Program.writeLine(website_database, website_input, key_dict)
+					Program.writeLine(url_database, url_input, key_dict)
+					Program.writeLine(username_database, username_input, key_dict)
 					if password_input == "/r":
-						Program.writeLine(password_database, generatePassword())
+						Program.writeLine(password_database, generatePassword(), key_dict)
 					else:
-						Program.writeLine(password_database, password_input)
+						Program.writeLine(password_database, password_input, key_dict)
 					print("action successful.")
 					print("password copied to clipboard")
 					input("return to continue: ")
@@ -158,6 +190,10 @@ def program(in_program):
 				elif confirmation == "N":
 					print("action cancelled.")
 					break
+	
+	# Makes key_dict global
+	global key_dict
+	key_dict = Program.openEncKey()
 
 	# The main program
 	def main():
