0a4d7b86b1cb1d1e813b302999571d40dbe286d5
Kaindra Djoemena <62385409+KaindraDjoemena@users.noreply.github.com>
2021-03-15 18:01:19 +0700

    Delete encrypter.py

--- password-manager/encrypter.py
+++
@@ -1,9 +0,0 @@
-from encryption_dict_file import *
-
-def encrypt(message):
-	encrypted_word = ""
-	char_list = list(message)
-	for char in char_list:
-		conv_char = conv_dict[char]
-		encrypted_word += conv_char
-	return encrypted_word
